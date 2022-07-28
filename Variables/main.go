4248633e4e036007170471fc52c7015e09e8b324
tousif644 <61720566+tousif644@users.noreply.github.com>
2022-07-28 06:29:45 +0600

    Finished variables

---
+++ Variables/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	var conferenceName = "Go conference"
+	fmt.Println(conferenceName)
+
+	var a = "apple"
+	fmt.Println(a)
+
+	var b,c = 1, 2
+	fmt.Println(b,c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int = 15
+	fmt.Println(e)
+}
